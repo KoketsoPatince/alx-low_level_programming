83107215b063989d1306e0d3524c2c8954bdc777
KoketsoPatince <kp.manonga@gmail.com>
2023-03-16 18:38:13 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - its the main function
+ * 
+ * Return: Always 0 when successful
+ *
+ */
+
+
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
