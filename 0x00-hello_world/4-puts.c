c483dd2c0df5eed96c87b62ef8dc76e3d3af06a3
KoketsoPatince <kp.manonga@gmail.com>
2023-03-16 17:58:26 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+include <stdi0.h>
+/**
+ * main - it is the main function
+ *
+ * Return: 0 
+ */
+
+int main (void)
+{
+        puts("\"Programming is like building a multilingual puzzle"):
+                return (0):
+
+}
