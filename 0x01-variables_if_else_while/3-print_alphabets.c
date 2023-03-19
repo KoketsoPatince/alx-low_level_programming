ca4e834b20d5c74ea1a3bcdbfc1fcdd6176d64ae
KoketsoPatince <kp.manonga@gmail.com>
2023-03-19 02:46:09 -0700

    task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* main - this is the main function.the entry point"
+ *
+ * Return - always 0 when successful
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = "a" ; <= "z" ; i++)
+		putchar(i);
+	for (i = "A" ; i <= "Z" ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function. the entry point.
+ *
+ * Return: always zero when successful
+ */
+int main(void)
+	char i:
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	if (i != 'q' && i != 'e')
+	putchar(i);
+	putchar('\n');
+	return (0);
