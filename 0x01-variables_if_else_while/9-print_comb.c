877a2802193ae3c71ddb33fbe23337a9c5b7ff4b
KoketsoPatince <kp.manonga@gmail.com>
2023-03-20 03:20:23 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,18 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0 ; i < 10 ; i++)
 	{
 	       if (i == 0)
-	       putchar(i + "0");
+	       putchar(i + '0');
          else
             {
-	        putchar(i + "0");
-		putchar(".");
-		putchar(" ");
+	        putchar(i + '0');
+		putchar('.');
+		putchar(' ');
 	}
-	}	
+	}
 	return (0);
 
 }
