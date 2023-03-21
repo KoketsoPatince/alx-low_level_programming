cf34c7ba4870e0635934f76cfb759350666bd4e4
KoketsoPatince <kp.manonga@gmail.com>
2023-03-21 09:07:05 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - the main function. the entry point
+ * _putchar: prints the alphaberts putchar
  *
  * Return: always 0 when successful
  */
 
 int main(void)
 {
-	putchar('\n')
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+
 }
