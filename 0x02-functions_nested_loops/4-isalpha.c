751ed4391652937ced70e52f1d2406a90700ad57
KoketsoPatince <kp.manonga@gmail.com>
2023-03-21 20:40:46 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - function to print lowercase character
+ * @c: the character to be printed
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
+}
