ee09d6d81c3123b4d0d5ee4ce513adef068696e5
KoketsoPatince <kp.manonga@gmail.com>
2023-03-23 06:28:58 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: the number to be checked
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{	
+		return (1);
+	}	
+	return (0);
+}
