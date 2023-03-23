2e97a95f647957acfc05fa604acc6f012024da36
KoketsoPatince <kp.manonga@gmail.com>
2023-03-22 17:12:49 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,23 +12,22 @@ long int j;
 long int n;
 long int next;
 
-        j = 1;
+j = 1;
+n = 2;
 
-        n = 2;
-
-        for (i = 1; i <= 98; ++i)
-        {
-                if (j != 20365011074)
-                {
-                        printf("%ld, ", j);
-                } else
-                {
-                        printf("%ld\n", j);
-                }
-                next = j + n;
-                j = n;
-                n = next;
-        }
-
-        return (0);
+for (i = 1; i <= 98; ++i)
+{
+if (j != 20365011074)
+{
+printf("%ld, ", j);
+}
+else
+{
+printf("%ld\n", j);
+}
+next = j + n;
+j = n;
+n = next;
+}
+return (0);
 }
