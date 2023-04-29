19eda5b72d671fcb52a86bfd53bb81e85b75795f
KoketsoPatince <kp.manonga@gmail.com>
2023-04-29 06:52:54 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+/*
+ * list_len - finds the elements in a linked list
+ * @h: pointer to the list 
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	int n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
