4a369953dbb623b79ac0a1113166023dd9175301
KoketsoPatince <kp.manonga@gmail.com>
2023-05-09 09:12:50 -0700

    task 15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o = 0;
-       	int w = 0;
-      	int n = 0;
+int o = 0;
+int w = 0;
+int n = 0;
 
 	if (filename == NULL)
 		return (-1);
